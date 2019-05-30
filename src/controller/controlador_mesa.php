a218b9fd876549a1d35b7fbdfb096194a8a78fbb
MayteZA <38025612+MayteZA@users.noreply.github.com>
2019-05-30 10:05:15 -0500

    .

--- src/controller/controlador_mesa.php
+++ src/controller/controlador_mesa.php
@@ -3,6 +3,7 @@
     $operacion = $_POST['operacion'];
     switch ($operacion) {
       case 'alta_mesa':
+            session_start();
         /*$comprobacion_correo=$conexion->query("select correo from clientes where correo='".$_POST['correo']."'");
 
         if ($result=$comprobacion_correo->fetch_assoc()) {
@@ -11,18 +12,19 @@
             /*$fechacierre=date('Y-m-d', strtotime($datos_evento['fechacierre']. ' + 7 days'));
             $fechaenvio=date('Y-m-d', strtotime($fechacierre.' + 7 days'));;*/
 
-
-            $stmt=$conexion->prepare("insert into eventos (idtipoevento,fechaevento) values (?,?)");
+            $stmt=$conexion->prepare("insert into eventos (idtipoevento,idcliente,fechaevento) values (?,?,?)");
+            echo $_POST['idtipoevento'];
             echo $_POST['fechaevento'];
+            echo $_SESSION['id_sesion_cliente'];
             //echo $stmt->error;
-            $stmt->bind_param("is", $_POST['idtipoevento'], $_POST['fechaevento']);
+            $stmt->bind_param("iis", $_POST['idtipoevento'], $_SESSION['id_sesion_cliente'], $_POST['fechaevento']);
             $stmt->execute();
             echo $stmt->error;
 
             if ($stmt->affected_rows) {
                 $msj="exito";
-                session_start();
-                $_SESSION['id_sesion_cliente']=$result['idcliente'];
+
+                $_SESSION['id_sesion_cliente']=$stmt->insert_id;
                 $_SESSION['nombre_cliente']=$result['nombre'];
                 $_SESSION['appat_cliente']=$result['appat'];
             } else {
